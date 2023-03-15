c30311101bca09c39559805b4bae931dc3ea21d8
Muh S <muhammadcashah@gmail.com>
2023-03-14 23:06:14 -0600

    Created login page, need to add actual google features --> need Karim/Jason or controller job

---
+++ src/view/src/components/Login.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import GoogleLogin  from 'react-google-login'
+import { FcGoogle } from 'react-icons/fc'
+// import { useNavigate } from 'react-router-dom'
+import video from '../assets/ink.mp4'
+import logo from '../assets/colors.png'
+
+const Login = () => {
+  return (
+    // Div to store the video that's running in the background
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video 
+          src={video}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+
+<div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt='logo'/>
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in
+                </button>
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
