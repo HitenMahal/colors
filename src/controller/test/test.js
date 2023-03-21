8a2abf8d0b3d230a259ad461118cebca5107b782
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2023-03-21 15:39:56 -0600

    Deliver react webpage with fastapi

---
+++ src/controller/test/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a basic React webpage.</p>
+    </div>
+  );
+}
+
+export default App;
