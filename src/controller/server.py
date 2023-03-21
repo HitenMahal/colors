d0e782ac95465736a3fdddfbbc383e2604cac4ca
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2023-03-21 17:04:52 -0600

    functions to get database info
    - user posts not working yet

--- src/controller/server.py
+++ src/controller/server.py
@@ -1,9 +1,17 @@
 
 from fastapi import FastAPI, APIRouter, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
-
+from google.cloud import firestore
 
 app = FastAPI()
+
+# Replace with the path to your service account key file
+key_path = "TOPSECRETDONOTSHARE.json"
+
+# Initialize the Firestore client
+
+#db = firestore.Client(project='colors-df7a2')
+db = firestore.Client.from_service_account_json(key_path)
 
 origins = [
     "http://localhost:3000",
@@ -21,4 +29,20 @@ app.add_middleware(
 
 @app.get("/", tags=["root"])
 def get_routes():
-    return ['/', '/login', '/home']
+    return
+
+@app.get("/")
+def get_users():
+    users = db.collection('User').get()
+    for user in users:
+        print(user.to_dict())
+    return users
+
+app.get("/")
+def get_user_posts(user_id):
+    posts = db.collection('User').where("ID", "==", user_id)
+    docs = posts.stream()
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()}')
+
+get_user_posts("firstID1")
