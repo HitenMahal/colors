7cb6601ccf69394991296e739ec50f644b852a15
Hiten M <hiten.mahalwar@ucalgary.ca>
2023-03-23 16:33:29 -0600

    Added react-hooks/constants/context

---
+++ src/view/src/hooks/use-user.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+import { getUserByUserId } from '../services/firebase';
+
+export default function useUser(userId) {
+    const [activeUser, setActiveUser] = useState();
+
+    useEffect( () => {
+        async function getUserObjByUserId(userId) {
+            const [user] = await getUserByUserId(userId);
+            setActiveUser( user || {} );
+        }
+        if (userId) {
+            getUserObjByUserId();
+        }
+    }, [userId]);
+
+    return { user: activeUser, setActiveUser };
+}
