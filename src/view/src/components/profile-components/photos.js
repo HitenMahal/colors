9ab3a8b3d301e820209c4442fb7b11e699152b09
Muh S <muhammadcashah@gmail.com>
2023-03-29 17:52:09 -0600

    Editted more CSS, merged with Hiten

--- src/view/src/components/profile-components/photos.js
+++ src/view/src/components/profile-components/photos.js
@@ -4,14 +4,16 @@ import Skeleton from 'react-loading-skeleton';
 export default function Photos( { photos } ) {
   console.log("PROFILE PHOTOS photos prop = ", photos, photos.length);
     return (
-        <div className="h-16 border-t border-gray-primary mt-12 pt-4">
-            <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
-                {!photos
+        <div className="h-16 mt-12 pt-4">
+            <div className="grid grid-cols-3 gap-4">
+                {!photos 
                 ? new Array(12).fill(0).map((_,i) => <Skeleton key={i} width={320} height={400}/>)
                 : photos.length > 0 
                 ? photos.map( (photo) => (
                     <div key={photo.docId} className="photo-tile">
-                        <img src={photo.imageSrc} alt={photo.caption}/>
+                        <div>
+                          <img src={photo.imageSrc} alt={photo.caption}/>
+                        </div>
                         <div className="absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex hidden">
                   <p className="flex items-center text-white font-bold">
                     <svg
